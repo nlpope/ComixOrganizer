09e017f25e9d5a72ad8df67c21c5af52b21e5b78
Noah Pope <npope@tutanota.com>
2023-10-18 13:44:48 -0400

    Initial Commit

---
+++ ComixOrganizer/ComixOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComixOrganizerApp.swift
+//  ComixOrganizer
+//
+//  Created by Noah Pope on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ComixOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
