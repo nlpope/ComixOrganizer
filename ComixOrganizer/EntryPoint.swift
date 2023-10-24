341927af22301597dc96875124a28e9d59f16333
Noah Pope <npope@tutanota.com>
2023-10-24 07:57:13 -0400

    changing @main's name to "EntryPoint" + adding a NavView for comics list

---
+++ ComixOrganizer/EntryPoint.swift
@@ -0,0 +1,22 @@
+//
+//  ComixOrganizerApp.swift
+//  ComixOrganizer
+//
+//  Created by Noah Pope on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct EntryPoint: App {
+    
+    //start @ Publisher Select View
+    //then on to titles (in alpha order) under said Publisher
+    //> filterable by character
+    //then on to runs & volumes in order of release date
+    var body: some Scene {
+        WindowGroup {
+            ComicListView()
+        }
+    }
+}
