6dccf0915d0237a975dbe15d1266b10eb8d9f7e0
Noah Pope <npope@tutanota.com>
2023-10-24 13:49:12 -0400

    creating Publisher Model & PublisherSelectView

---
+++ ComixOrganizer/Models/Publisher.swift
@@ -0,0 +1,8 @@
+//
+//  Publisher.swift
+//  ComixOrganizer
+//
+//  Created by Noah Pope on 10/24/23.
+//
+
+import Foundation
---
+++ ComixOrganizer/Views/PublisherSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  PublisherSelectView.swift
+//  ComixOrganizer
+//
+//  Created by Noah Pope on 10/24/23.
+//
+
+import SwiftUI
+
+struct PublisherSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PublisherSelectView()
+}
