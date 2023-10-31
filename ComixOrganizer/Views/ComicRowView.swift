2dbe30ffa57221bd3cf986d7f6ba1a4fa39fdb07
Noah Pope <npope@tutanota.com>
2023-10-31 08:07:45 -0400

    adding ComicRowView

---
+++ ComixOrganizer/Views/ComicRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ComicRowView.swift
+//  ComixOrganizer
+//
+//  Created by Noah Pope on 10/31/23.
+//
+
+import SwiftUI
+
+struct ComicRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComicRowView()
+}
